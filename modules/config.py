c59c2817b7128600121f26bf627ad9293abe430c
Miro <kolecani@gmail.com>
2025-04-18 13:57:06 +0100

    test

--- modules/config.py
+++ modules/config.py
@@ -1,12 +1,18 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv('/home/miro/envs.env')
+load_dotenv('/home/miro/envs/.env')
 
-# print(repr(os.getenv('DATABASE_HOST')))
-# print(repr(os.getenv('DATABASE_USER')))
-# print(repr(os.getenv('DATABASE_PASSWORD')))
-# print(repr(os.getenv('DATABASE_NAME')))
+print(repr(os.getenv('DATABASE_HOST')))
+print(repr(os.getenv('DATABASE_USER')))
+print(repr(os.getenv('DATABASE_PASSWORD')))
+print(repr(os.getenv('DATABASE_NAME')))
+
+
+print("[DEBUG] BROKER_PORT:", os.getenv("BROKER_PORT"))
+print("[DEBUG] CWD:", os.getcwd())
+print("[DEBUG] FILE:", __file__)
+
 
 DATABASE_HOST = os.getenv('DATABASE_HOST')
 DATABASE_USER = os.getenv('DATABASE_USER')
@@ -22,6 +28,10 @@ ACCES_TOKEN_EXPIRE_TIME = os.getenv('ACCES_TOKEN_EXPIRE_TIME')
 MQTT_USERNAME = os.getenv('MQTT_USERNAME')
 MQTT_PASSWORD = os.getenv('MQTT_PASSWORD')
 BROKER_ADDRESS = os.getenv('BROKER_ADDRESS')
-BROKER_PORT = int(os.getenv('BROKER_PORT'))
+BROKER_PORT = os.getenv('BROKER_PORT')
 
 # print(DATABASE_URL)
+print(MQTT_USERNAME)
+print(MQTT_PASSWORD)
+print(BROKER_ADDRESS)
+print(BROKER_PORT)
